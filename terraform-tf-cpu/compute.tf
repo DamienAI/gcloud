17dbfb4d48773517ee1c02944c80142395dedc9e
Damien <damiendr@gmail.com>
2018-07-11 21:31:12 -0400

    Add terraform example for AI VM using tensorflow CPU

---
+++ terraform-tf-cpu/compute.tf
@@ -0,0 +1,25 @@
+# Create a new instance
+resource "google_compute_instance" "default" {
+  name         = "tf-latest-cpu"
+  machine_type = "f1-micro"
+  zone         = "${var.TF_VAR_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "tf-latest-cpu"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+output "instance_id" {
+  value = "${google_compute_instance.default.self_link}"
+}
---
+++ terraform-tf-cpu/variables.tf
@@ -0,0 +1,10 @@
+variable "TF_VAR_region" {
+  default = "us-central1"
+}
+
+variable "TF_VAR_zone" {
+  default = "us-west1-a"
+}
+
+variable "TF_VAR_project_name" {}
+variable "TF_creds" {}
